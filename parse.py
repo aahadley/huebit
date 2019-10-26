343eafd1bd726e9d5309826c448730552cc8bc11
PromAtlas <dylan.ochoa2323@gmail.com>
2019-10-26 04:08:54 -0400

    Rename variables and correct logic

--- parse.py
+++ parse.py
@@ -1,8 +1,8 @@
 import math
 import binascii
 f = open("sample.txt","r", encoding="utf-8")
-arrays = []
-tempar = []
+multidarray = []
+onedarray = []
 if f.mode == 'r':
     f1 = f.readlines()
 linenum = 1
@@ -10,18 +10,18 @@ linenum = 1
 for x in f1:
 
     if "entangled" in x:
-        arrays.append([-1,-1])
+        multidarray.append([-1,-1])
         continue
     elif linenum % 3 != 1:
-        index1 = x.find("\t") +2
+        index1 = x.find("\t") +1
         index2 = x.find("i") + 1
-        sub = x[index1:index2]
-        brokesub = sub.split()
-        mynum = float(brokesub[0]) + float(brokesub[2])
-        tempar.append(mynum)
+        substring = x[index1:index2]
+        splitsubstring = substring.split()
+        calc = float(splitsubstring[0]) + float(splitsubstring[2]) *1j
+        onedarray.append(calc)
     if linenum %3 == 0:
-        print(tempar)
-        arrays.append(tempar)
-        tempar = []
+        onedarray[0],onedarray[1] = onedarray[1],onedarray[0]
+        multidarray.append(onedarray)
+        onedarray = []
     linenum += 1
-print(arrays)
+print(multidarray)
